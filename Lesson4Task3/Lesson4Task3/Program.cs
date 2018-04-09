f562d411200e21c6d25926d62898aba2f7a2d4ed
brestows <brestows@gmail.com>
2018-04-10 01:29:29 +0200

    lesson 4 task 3

---
+++ Lesson4Task3/Lesson4Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>()
+            {
+                {"four", 4 },
+                {"two",  2 },
+                { "one", 1 },
+                {"three",3 },
+            };
+            var d = dict.OrderBy(delegate (KeyValuePair<string, int> item) { return item.Value; });
+            var l = dict.OrderBy(e => e.Value);
+            
+            //Исходный вариант
+            foreach (var item in d)
+                Console.WriteLine($"{item.Key} - {item.Value}");
+
+            Console.WriteLine();
+
+            //Лямда вариант
+            foreach (var item in d)
+                Console.WriteLine($"{item.Key} - {item.Value}");
+
+            Console.ReadKey();
+        }
+    }
+}
