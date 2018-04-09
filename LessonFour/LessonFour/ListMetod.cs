98fa5e68760d5a766a1fa8b8e636329eec3f1d54
brestows <brestows@gmail.com>
2018-04-10 01:09:01 +0200

    4 урок задание 2

---
+++ LessonFour/LessonFour/ListMetod.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonFour
+{
+    /// <summary>
+    /// Статический класс, с методами обработки коллекций
+    /// </summary>
+    public static class ListMetod
+    {
+        /// <summary>
+        /// Метод подсчета сопадений элементов коллекции 
+        /// </summary>
+        /// <param name="lst">Анализируемая коллекция</param>
+        public static void intListElementMatch(List<int> lst)
+        {
+            SortedDictionary<int, int> dic = new SortedDictionary<int, int>();
+            foreach (int i in lst)
+                dic[i] = (dic.ContainsKey(i)) ? ++dic[i] : 1;
+            foreach (KeyValuePair<int, int> item in dic)
+                Console.WriteLine($"{item.Key} найдено сопадений: {item.Value}.");
+        }
+        /// <summary>
+        /// Не отличается от метода intListElentMatch
+        /// </summary>
+        /// <typeparam name="T">Тип обрабатываемой коллекции</typeparam>
+        /// <param name="lst">Анализируемая коллекция</param>
+        public static void tmpListElementMatch<T> (List<T> lst)
+        {
+            SortedDictionary<T, int> dic = new SortedDictionary<T, int>();
+            foreach (T i in lst)
+                dic[i] = (dic.ContainsKey(i)) ? ++dic[i] : 1;
+            foreach (KeyValuePair<T, int> item in dic)
+                Console.WriteLine($"{item.Key} найдено сопадений: {item.Value}.");
+        }
+        /// <summary>
+        /// Анализируем коллекцию спомощью linq
+        /// </summary>
+        /// <param name="lst">Анализируемая коллекция</param>
+        public static void lnqListElementMatch(List<int> lst)
+        {
+            SortedDictionary<int, int> dic = new SortedDictionary<int, int>();
+            foreach (int i in lst)
+            {
+                var tmp = from n in lst where n == i select n;
+                if (!dic.ContainsKey(i))  dic[i] = tmp.Count();
+            }
+            foreach (KeyValuePair<int, int> item in dic)
+                Console.WriteLine($"{item.Key} найдено сопадений: {item.Value}.");
+        }
+    }
+}
--- LessonFour/LessonFour/Program.cs
+++ LessonFour/LessonFour/Program.cs
@@ -10,7 +10,32 @@ namespace LessonFour
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("exit");
+            //Кол-во элементов в списке
+            int maxElement = 30;
+            Random rnd = new Random();
+            // Заполняем список случайными числами от  0 до 10
+            List<int> RandomList = Enumerable.Range(0, maxElement).Select(x => rnd.Next(0,10)).ToList();
+            foreach (int i in RandomList)
+                Console.Write($"{i}, ");
+            Console.WriteLine("");
+            // Задание А
+            ListMetod.intListElementMatch(RandomList);
+            List<string> CityList = new List<string>()
+            {
+                "Париж","Лондон","Минск", "Варшава","Москва","Ливерпуль","Лисабон","Киев", "Кишинев",
+                "Брест","Лондон","Минск", "Варшава","Кравков","Мадрид","Лисабон","Киев", "Берлин",
+                "Париж","Санкт-Петербург","Минск", "Варшава","Москва","Мадрид","Лисабон","Киев", "Кишинев",
+                "Брест","Лондон","Минск", "Варшава","Москва","Мадрид","Лисабон","Киев", "Кишинев",
+                "Париж","Лондон","Минск", "Варшава","Москва","Барселона","Лисабон","Киев", "Кишинев",
+                "Париж","Манчестер","Минск", "Гданьск","Москва","Санкт-Петербург","Лисабон","Киев", "Сочи",
+
+            };
+            // Задание Б
+            ListMetod.tmpListElementMatch(CityList);
+            //Задание В
+            ListMetod.lnqListElementMatch(RandomList);
+
+            Console.ReadKey();
         }
     }
 }
